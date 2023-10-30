b45dd65be4f746f49cf4e21237c57d9c3bde2559
Alex Dixon <krios_global@protonmail.ch>
2023-10-30 12:00:24 +0100

    Added structure and placeholders for api

---
+++ provider_portal/api/api.py
@@ -0,0 +1,14 @@
+from provider_portal.api import bp
+
+
+@bp.route('meter-create', methods=['POST'])
+def create_meter():
+    pass
+
+@bp.route('meter-measurements', methods=['GET'])
+def meter_measurements():
+    pass
+
+@bp.route('meter-delete', methods=['DELETE'])
+def delete_meter():
+    pass
