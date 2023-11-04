1edbfe635fe7fc248261c5e0dc7873c74afc18cc
Joshua <>
2023-11-04 23:45:39 +0100

    Compatability restored

--- provider_portal/app/api/smartmeter_api/smartmeter_api.py
+++ provider_portal/app/api/smartmeter_api/smartmeter_api.py
@@ -24,6 +24,7 @@ class SmartmeterAPI:
             return False
 
     def add_measurements(self, datapoints):
+        print(datapoints)
         for datapoint in datapoints:
             timestamp = datapoint["timestamp"]
             value = datapoint["value"]
