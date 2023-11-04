86e6d1f0a8a2d19da2c97dae8b9f9ccc3b3292d0
Alex Dixon <krios_global@protonmail.ch>
2023-11-04 11:40:03 +0100

    Reworked imports

--- provider_portal/app/__init__.py
+++ provider_portal/app/__init__.py
@@ -4,13 +4,13 @@ from flask import Flask
 def create_app():
     app = Flask(__name__)
 
-    from provider_portal.app.api.customer_api import customer_api_blueprint as customer_api_routes
+    from app.api.customer_api import customer_api_blueprint as customer_api_routes
     app.register_blueprint(customer_api_routes, url_prefix='/v1/provider')
 
-    from provider_portal.app.api.smartmeter_api import smartmeter_api_blueprint as smartmeter_api_routes
+    from app.api.smartmeter_api import smartmeter_api_blueprint as smartmeter_api_routes
     app.register_blueprint(smartmeter_api_routes, url_prefix='/v1/smartmeter')
 
-    from provider_portal.app.api.admin_api import admin_api_blueprint as admin_api_routes
+    from app.api.admin_api import admin_api_blueprint as admin_api_routes
     app.register_blueprint(admin_api_routes, url_prefix='/v1/smartmeter')
 
     return app
