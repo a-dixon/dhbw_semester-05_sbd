a9936322626189ada054a1101e4c062e39211c95
Alex Dixon <krios_global@protonmail.ch>
2023-11-04 13:57:11 +0100

    Added config structure

--- provider_portal/app/__init__.py
+++ provider_portal/app/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
+from config import Config
 
 
 def create_app():
     app = Flask(__name__)
+    app.config.from_object(Config)
 
     from app.api.customer_api import customer_api_blueprint as customer_api_routes
     app.register_blueprint(customer_api_routes, url_prefix='/v1/provider')
