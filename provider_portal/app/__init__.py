f3645cc23c7bb59e0e3e89b42f2336c75dd06d7e
Alex Dixon <krios_global@protonmail.ch>
2023-11-05 11:47:56 +0100

    Renamed Config class

--- provider_portal/app/__init__.py
+++ provider_portal/app/__init__.py
@@ -1,11 +1,11 @@
 from flask import Flask
-from config.config import Config
+from config.config import MySQLConfig
 #from flask_sqlalchemy import SQLAlchemy
 
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object(Config)
+   app.config.from_object(MySQLConfig)
     #mysql_db = SQLAlchemy(app)
 
     #from app.db.mysql import mysql
