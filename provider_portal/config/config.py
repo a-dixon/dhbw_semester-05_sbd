36ba78893626ce9246f573bb26953bd8d10fc66c
Alex Dixon <krios_global@protonmail.ch>
2023-11-08 11:50:42 +0100

    Removed database uri

--- provider_portal/config/config.py
+++ provider_portal/config/config.py
@@ -5,4 +5,5 @@ SERVER_KEY = "config/certificates/server_certificates/server-private-key.pem"
 CA_CERT = "config/certificates/root_ca/ca-public-key.pem"
 
 class MySQLConfig(object):
-    SQLALCHEMY_DATABASE_URI = 'mysql+mysqldb://provider_user:db_password@mysql/provider_db'
+    #SQLALCHEMY_DATABASE_URI = 'mysql+mysqldb://provider:xEMRpr32b7Xg8nNCWNakgnDrSja8b5@10.0.1.40:3306/provider'
+    pass
