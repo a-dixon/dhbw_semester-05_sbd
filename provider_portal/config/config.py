2fb9ab35098dffb977816f61542d38c36c5199a7
Joshua <>
2023-11-08 17:13:54 +0100

    Added Config Parameters

--- provider_portal/config/config.py
+++ provider_portal/config/config.py
@@ -4,5 +4,10 @@ SERVER_CERT = "config/certificates/server_certificates/server-public-key.pem"
 SERVER_KEY = "config/certificates/server_certificates/server-private-key.pem"
 CA_CERT = "config/certificates/root_ca/ca-public-key.pem"
 
+INFLUX_URL = "http://10.0.1.30:8086"
+INFLUX_TOKEN = "DzFwd-VxKfwt4Y20okuIDbolk3X8bcsGLOMDM0mp2IsdImr-Uji5JrclSyHmuUo-QLgIUmyQYJXzRhqKjNmmXQ=="
+INFLUX_BUCKET = "smartmeter"
+INFLUX_PROVIDER = "provider"
+
 class MySQLConfig(object):
     SQLALCHEMY_DATABASE_URI = 'mysql+mysqldb://provider_user:db_password@mysql/provider_db'
