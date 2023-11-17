dda69189f448019a51b8f7c6f518a91b1779ada3
Joshua <>
2023-11-17 19:56:00 +0100

    Added feature auto off smartmeter

--- smart_meter/run.py
+++ smart_meter/run.py
@@ -6,13 +6,11 @@ import time
 from config import config
 from app.smartmeter import smartmeter
 
-smartmeters = []
-
+smartmeters = {}
 
 def _run_smart_meter_for_uid(uid):
     smart_meter = smartmeter.SmartMeter(uid)
     smart_meter.run_smart_meter(60, 1)
-
 
 def start_smart_meters_in_parallel(path):
     for subdir, _, _ in os.walk(path):
@@ -20,24 +18,35 @@ def start_smart_meters_in_parallel(path):
             uid = subdir.split("/")[-1]
             thread = threading.Thread(target=_run_smart_meter_for_uid, args=(uid,))
             print("Smartmeter mit der UID: " + uid + " gestartet", file=sys.stderr)
-            smartmeters.append(uid)
+            smartmeters[uid] = thread
+            thread.do_run = True
             thread.start()
 
+def check_for_changed_smart_meters(path):
+    global smartmeters
+    current_uids = set(subdir.split("/")[-1] for subdir, _, _ in os.walk(path) if subdir.startswith(path + "/"))
+    existing_uids = set(smartmeters.keys())
 
-def check_for_new_smart_meters(path):
-    for subdir, _, _ in os.walk(path):
-        if subdir.startswith(path + "/"):
-            uid = subdir.split("/")[-1]
-            if uid not in smartmeters:
-                thread = threading.Thread(target=_run_smart_meter_for_uid, args=(uid,))
-                print("Smartmeter mit der UID: " + uid + " gestartet", file=sys.stderr)
-                smartmeters.append(uid)
-                thread.start()
+    # Check for new smart meters
+    new_uids = current_uids - existing_uids
+    for uid in new_uids:
+        thread = threading.Thread(target=_run_smart_meter_for_uid, args=(uid,))
+        print("Neuer Smartmeter mit der UID: " + uid + " gestartet", file=sys.stderr)
+        smartmeters[uid] = thread
+        thread.start()
 
+    # Check for deleted smart meters
+    deleted_uids = existing_uids - current_uids
+    for uid in deleted_uids:
+        thread = smartmeters.pop(uid)
+        thread.do_run = False
+        thread.join()  # Wait for the thread to finish
+
+        print("Smartmeter mit der UID: " + uid + " beendet", file=sys.stderr)
 
 if __name__ == '__main__':
     start_smart_meters_in_parallel(config.CERT_DIRECTORY)
 
     while True:
         time.sleep(5)
-        check_for_new_smart_meters(config.CERT_DIRECTORY)
+        check_for_changed_smart_meters(config.CERT_DIRECTORY)
