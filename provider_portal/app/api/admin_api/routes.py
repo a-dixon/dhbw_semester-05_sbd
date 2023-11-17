e873a7f453466d474d3eefc9ef7542754665cc77
Alex Dixon <krios_global@protonmail.ch>
2023-11-17 17:36:46 +0100

    Created admin api route

--- provider_portal/app/api/admin_api/routes.py
+++ provider_portal/app/api/admin_api/routes.py
@@ -1,6 +1,28 @@
+from flask import request, json
+from .admin_api import AdminAPI
+from .response import Response
 from . import admin_api_blueprint as bp
 
 
-@bp.route('test', methods=['POST'])
-def create_meter():
-    pass
+@bp.route('customer-create', methods=['POST'])
+def create_customer_portal():
+    ''' Route to create customer portal from admin cli.'''   
+    # --- Extract data from request body ---
+    data = json.loads(request.data)
+    api_key = data['api_key']
+    username = data['username']
+
+    # --- Instantiate admin api class ---
+    api = AdminAPI(api_key=api_key, username=username)
+    auth_status = api.authenticate_admin_user()
+
+    if auth_status:
+
+        try:
+            customer_UID, customer_api_key = api.create_customer()
+            res = {"message": "success_create_customer", "customer_UID": customer_UID, "customer_api_key": customer_api_key}
+            return Response(dict=res).create_response()
+        
+        except:
+            res = {"message": "error_create_customer"}
+            return Response(dict=res).create_response()
