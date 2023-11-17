01937add412742e764ad4311cb5486266480f76e
Alex Dixon <krios_global@protonmail.ch>
2023-11-17 10:52:46 +0100

    Amended routes

--- provider_portal/app/api/customer_api/routes.py
+++ provider_portal/app/api/customer_api/routes.py
@@ -1,6 +1,7 @@
 from flask import request, json, jsonify
 from . import customer_api_blueprint as bp
-from . import customer_api
+from customer_api import CustomerAPI
+from response import Response
 
 
 @bp.route('meter-create', methods=['POST'])
@@ -14,12 +15,20 @@ def create_meter():
     customer_UID = data['customerUID']
 
     # Initiate customer api class
-    api = customer_api.CustomerAPI(customer_UID=customer_UID, api_key=api_key)
+    api = CustomerAPI(customer_UID=customer_UID, api_key=api_key)
+    auth_status = api.authenticate_customer_portal()
 
+    if auth_status:
+        
+        try:
+            meter_UID = api.create_meter()
+            create_status = True
+            delete_status = False
+        except:
+            create_status = False
+            delete_status = False
     
-    if not api == False:
-        meter_UID = api.create_meter()
-
+    return Response([auth_status, create_status, delete_status], meter_UID=meter_UID).to_response()
 
 
 @bp.route('meter-measurements', methods=['GET'])
@@ -53,5 +62,10 @@ def delete_meter():
     customer_UID = data['customerUID']
     meter_UID = data['meterUID']
 
-    api = customer_api(customer_UID=customer_UID, api_key=api_key)
+    api = CustomerAPI(customer_UID=customer_UID, api_key=api_key)
+    auth_status = api.authenticate_customer_portal()
+
+    if auth_status:
+        pass
+
     pass
