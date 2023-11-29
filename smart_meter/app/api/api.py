1c6f7b333f84d486587e801140133b9baaac29dc
Joshua <>
2023-11-29 22:38:06 +0100

    Added commentary and docstrings to smart meter

--- smart_meter/app/api/api.py
+++ smart_meter/app/api/api.py
@@ -2,18 +2,36 @@ import requests
 import json
 from config import config
 
+
 class APIHandler:
     def __init__(self, api_url, uid):
+        """
+        Initialize the APIHandler instance.
+
+        Parameters:
+        - api_url (str): The URL of the API endpoint.
+        - uid (str): The unique identifier associated with the meter.
+        """
         self._api_url = api_url
         self._uid = uid
 
     def send_data(self, data):
+        """
+        Send data to the specified API endpoint.
+
+        Parameters:
+        - data (dict): The data to be sent.
+
+        Returns:
+        - bool: True if the data was successfully sent, False otherwise.
+        """
         headers = {
             'Content-Type': 'application/json'
         }
 
-        # certificates = (f"{config.CERT_DIRECTORY}/{self._uid}/client.crt", f"{config.CERT_DIRECTORY}/{self._uid}/client.key")
-        certificates = (f"{config.CertificateConfig.CERT_DIRECTORY}/{self._uid}/client-public-key.pem", f"{config.CertificateConfig.CERT_DIRECTORY}/{self._uid}/client-private-key.pem")
+        # Paths to client certificates
+        certificates = (f"{config.CertificateConfig.CERT_DIRECTORY}/{self._uid}/client-public-key.pem",
+                        f"{config.CertificateConfig.CERT_DIRECTORY}/{self._uid}/client-private-key.pem")
 
         post_data = {
             "meterUID": self._uid,
@@ -21,14 +39,21 @@ class APIHandler:
         }
 
         if not certificates:
+            # If certificates are not available, return False
             return False
+
         try:
-            response = requests.post(self._api_url, data=json.dumps(post_data), headers=headers, cert=certificates, verify=config.CertificateConfig.ROOT_CA_PEM)
-            # response = requests.post(self._api_url, data=json.dumps(post_data), headers=headers, cert=certificates, verify=False)
+            # Send a POST request to the API endpoint
+            response = requests.post(self._api_url, data=json.dumps(post_data),
+                                     headers=headers, cert=certificates, verify=config.CertificateConfig.ROOT_CA_PEM)
+
             if response.status_code == 200:
+                # If the response status code is 200, consider it a success
                 return True
             else:
+                # Otherwise, consider it a failure
                 return False
         except Exception as e:
+            # Handle exceptions and return False
             print(e)
             return False
--- smart_meter/app/smartmeter/smartmeter.py
+++ smart_meter/app/smartmeter/smartmeter.py
@@ -8,18 +8,39 @@ from config import config
 
 class SmartMeter:
     def __init__(self, uid):
+        """
+        Initialize the SmartMeter instance.
+
+        Parameters:
+        - uid (str): The unique identifier associated with the meter.
+        """
         self._consumption = []
         self._uid = uid
         self._api = APIHandler(config.APIConfig.API_URL, uid)
 
     @staticmethod
     def _generate_consumption(current_time):
+        """
+        Generate a random consumption value with a slight variation.
+
+        Parameters:
+        - current_time (datetime): The current timestamp.
+
+        Returns:
+        - float: Randomized consumption value.
+        """
         consumption = 1
         random_factor = random.uniform(-0.1, 0.1)
         randomized_consumption = consumption + (consumption * random_factor)
         return randomized_consumption
 
     def _write_consumption(self, time_between_datapoints):
+        """
+        Generate and append consumption data if enough time has passed.
+
+        Parameters:
+        - time_between_datapoints (int): Time interval between each data point.
+        """
         current_time = datetime.now()
 
         if self._consumption:
@@ -33,9 +54,21 @@ class SmartMeter:
             self._consumption.append((current_time, consumption))
 
     def _delete(self, timestamp):
+        """
+        Delete consumption data points older than the specified timestamp.
+
+        Parameters:
+        - timestamp (datetime): The timestamp used as a reference for deletion.
+        """
         self._consumption = [(t, v) for t, v in self._consumption if t > timestamp]
 
     def _transfer(self, datapoints_until_send):
+        """
+        Transfer consumption data to the API endpoint.
+
+        Parameters:
+        - datapoints_until_send (int): Number of data points to transfer at once.
+        """
         data_list = []
         for i in range(0, datapoints_until_send):
             timestamp, value = self._consumption[i]
@@ -52,6 +85,13 @@ class SmartMeter:
             self._delete(last_timestamp)
 
     def run_smart_meter(self, datapoints_unitl_send, time_between_datapoints):
+        """
+        Run the smart meter simulation.
+
+        Parameters:
+        - datapoints_unitl_send (int): Number of data points to accumulate before sending to the API.
+        - time_between_datapoints (int): Time interval between each data point generation.
+        """
         while getattr(threading.current_thread(), "do_run", True):
             self._write_consumption(time_between_datapoints)
             if len(self._consumption) > datapoints_unitl_send:
@@ -59,4 +99,10 @@ class SmartMeter:
             time.sleep(0.2)
 
     def get_uid(self):
+        """
+        Get the unique identifier associated with the smart meter.
+
+        Returns:
+        - str: The smart meter's unique identifier.
+        """
         return self._uid
