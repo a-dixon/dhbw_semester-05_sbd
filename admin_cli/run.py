8655cae172c5e2842d08b72c77dee372878766db
Joshua <>
2023-12-01 19:14:35 +0100

    Refactored admin cli

--- admin_cli/run.py
+++ admin_cli/run.py
@@ -1,17 +1,15 @@
-import click
-import requests
-from colorama import Fore, Style, init
 import os
-from click.termui import style
 
-init(autoreset=True)
+import click
+from click import style
+from colorama import Fore, Style
 
-# Globale Variablen für die Anmeldedaten
-username = None
-api_key = None
+from app.client_api import APIClient
+
 
 def clear_screen():
     os.system('cls' if os.name == 'nt' else 'clear')
+
 
 def get_terminal_size():
     try:
@@ -20,57 +18,21 @@ def get_terminal_size():
     except:
         return 24, 80  # Standardgröße
 
+
 def center_text(text, width):
     return (" " * ((width - len(text)) // 2)) + text
 
-def api_request(endpoint, data):
-    url = f"https://10.0.1.10:8090/v1/admin/{endpoint}"
-    try:
-        response = requests.post(url, json=data, verify="config/certificates/root_ca/ca-public-key.pem")
-        response.raise_for_status()
-        return response.json()
-    except requests.exceptions.HTTPError as errh:
-        click.echo(Fore.RED + f"HTTP Error: {errh}")
-    except requests.exceptions.RequestException as err:
-        click.echo(Fore.RED + f"Anfragefehler: {err}")
-    except ValueError as verr:
-        click.echo(Fore.RED + f"JSON Dekodierungsfehler: {verr}")
-    return {}
-
-def login():
-    global username, api_key
-
-    click.echo(Fore.GREEN + center_text("===== ANMELDUNG =====", terminal_width))
-    username = click.prompt(Fore.CYAN + "Username")
-    api_key = click.prompt("API Key")
-    click.echo(Fore.GREEN + "Anmeldung erfolgreich!")
-
-def neue_customer_portale():
-    global username, api_key
-
-    if username is None or api_key is None:
-        click.echo(Fore.RED + "Fehler: Nicht angemeldet. Das Programm wird beendet.")
-        return
-
-    data = {"username": username, "api_key": api_key}
-    response = api_request("customer-create", data)
-
-    if "customer_UID" in response and "customer_api_key" in response:
-        click.echo(Fore.YELLOW + "Kunde erfolgreich erstellt:")
-        click.echo(f"Kunden-UID: {response['customer_UID']}")
-        click.echo(f"Kunden-API-Key: {response['customer_api_key']}")
-    else:
-        click.echo(Fore.RED + f"Fehler: {response.get('message', 'Unbekannter Fehler')}")
 
 if __name__ == "__main__":
     terminal_height, terminal_width = get_terminal_size()
 
+    api = APIClient(base_url="https://10.0.1.10:8090/v1/admin/", cert_path="config/certificates/root_ca/ca-public-key.pem")
+
     clear_screen()
     login_text = center_text("===== ANMELDUNG =====", terminal_width)
     click.echo(style(login_text, fg="green"))
-    username = click.prompt(style(Fore.CYAN + "Username", fg="green"))
-    api_key = click.prompt(style("API Key", fg="green"))
-    click.echo(style(Fore.GREEN + "Anmeldung erfolgreich!", fg="green"))
+    api.login()
+    click.echo(style(Fore.GREEN + "Die Session wurde initiiert", fg="green"))
     click.pause(info="Drücken Sie eine Taste, um fortzufahren...")
 
     while True:
@@ -79,14 +41,25 @@ if __name__ == "__main__":
         menu_indent = (terminal_width - menu_width) // 2
         click.echo(" " * menu_indent + Fore.BLUE + "========== MENÜ ==========")
         click.echo(" " * menu_indent + "1. " + Fore.YELLOW + "Neue Kundenportale hinzufügen")
-        click.echo(" " * menu_indent + "2. " + Fore.RED + "Beenden" + Style.RESET_ALL)
+        click.echo(" " * menu_indent + "2. " + Fore.YELLOW + "Kundenportale auflisten")
+        click.echo(" " * menu_indent + "3. " + Fore.YELLOW + "Kundenportal löschen")
+        click.echo(" " * menu_indent + "4. " + Fore.YELLOW + "Smartmeter auflisten")
+        click.echo(" " * menu_indent + "5. " + Fore.RED + "Beenden" + Style.RESET_ALL)
         click.echo(" " * menu_indent + "=" * menu_width)
 
-        choice = click.prompt(Fore.CYAN + "Bitte wählen Sie eine Option (1-2)", type=int)
+        choice = click.prompt(Fore.CYAN + "Bitte wählen Sie eine Option (1-5)")
 
-        if choice == 1:
-            neue_customer_portale()
-        elif choice == 2:
+        if choice == "1":
+            api.new_customer_portals()
+        elif choice == "2":
+            api.list_customer_portals()
+        elif choice == "3":
+            customer_UID = input("Bitte geben Sie die Kunden-UID ein: ")
+            api.delete_customer_portal(customer_UID)
+        elif choice == "4":
+            customer_UID = input("Bitte geben Sie die Kunden-UID ein: ")
+            api.list_smart_meters_of_customer(customer_UID)
+        elif choice == "5":
             click.echo(Fore.RED + "Programm wird beendet.")
             break
         else:
